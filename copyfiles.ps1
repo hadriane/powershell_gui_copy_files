7ff590cb900e7676b06402d2835c34f2111a31f3
hadriane <60728086+hadriane@users.noreply.github.com>
2020-04-01 21:10:59 +0800

    Update copyfiles.ps1

--- copyfiles.ps1
+++ copyfiles.ps1
@@ -12,35 +12,48 @@ function GenerateForm {
 	
 	# Initializing a form
 	$basicForm = New-Object System.Windows.Forms.Form
-	$basicForm.Size = '450,300'
+	$basicForm.Size = '450,315'
 	$basicForm.StartPosition = 'CenterScreen'
 	$basicForm.Text = 'Copy files from source to destination'
 
-
+	
+	# Labeling source folder text box
+	$labelSourceFolder = New-Object System.Windows.Forms.Label
+	$labelSourceFolder.Location = '23,23'
+	$labelSourceFolder.Size = '300,23'
+	$labelSourceFolder.Text = 'Source folder'
+	
 	# Defining source folder text box
-	$textBoxsourceFolder = New-Object System.Windows.Forms.TextBox
-	$textBoxsourceFolder.Text = 'Select the source folder'
-	$textBoxsourceFolder.Location = '23,23'
-	$textBoxsourceFolder.Size = '300,23'
+	$textBoxSourceFolder = New-Object System.Windows.Forms.TextBox
+	$textBoxSourceFolder.Location = '23,46'
+	$textBoxSourceFolder.Size = '300,23'
+	$textBoxSourceFolder.Text = 'Select the source folder'
 	
 	# Defining source folder select button 
-	$buttonSelectsourceFolder = New-Object System.Windows.Forms.Button
-	$buttonSelectsourceFolder.Text = 'Select'
-	$buttonSelectsourceFolder.Location = '350,23'
+	$buttonSelectSourceFolder = New-Object System.Windows.Forms.Button
+	$buttonSelectSourceFolder.Location = '350,46'
+	$buttonSelectSourceFolder.Text = 'Source'
 	
 	# Assigning source folder select button
 	$browsesourceFolder = New-Object System.Windows.Forms.FolderBrowserDialog
-	$buttonSelectsourceFolder.Add_Click({
+	$buttonSelectSourceFolder.Add_Click({
 		$browsesourceFolder.ShowDialog()
-		$textBoxsourceFolder.Text = $browsesourceFolder.SelectedPath
+		$textBoxSourceFolder.Text = $browsesourceFolder.SelectedPath
 	})
-	$textBoxsourceFolder.ReadOnly = $true
+	$textBoxSourceFolder.ReadOnly = $true
 
 
+	# Labeling source folder text box
+	$labelDropDown = New-Object System.Windows.Forms.Label
+	$labelDropDown.Location = '23,92'
+	$labelDropDown.Size = '300,23'
+	$labelDropDown.Text = 'File Extension'
+	
 	# Defining drop down box
 	$boxDropDown = New-Object System.Windows.Forms.ComboBox
-	$boxDropDown.Location = '23,69'
-	$boxDropDown.Size = '130,30'
+	$boxDropDown.Location = '23,115'
+	$boxDropDown.Size = '130,65'
+	$boxDropDown.Text = 'Select file extension'
 	
 	# Assigning items to drop down box
 	ForEach ($Item in $dropDownArray) {
@@ -48,17 +61,23 @@ function GenerateForm {
 	}
 	
 	
+	# Labeling destination folder text box
+	$labelDestinationFolder = New-Object System.Windows.Forms.Label
+	$labelDestinationFolder.Location = '23,161'
+	$labelDestinationFolder.Size = '300,23'
+	$labelDestinationFolder.Text = 'Destination folder'
+	
 	# Defining destination folder text box
 	$textBoxDestinationFolder = New-Object System.Windows.Forms.TextBox
-	$textBoxDestinationFolder.Text = 'Select the destination folder'
-	$textBoxDestinationFolder.Location = '23,115'
+	$textBoxDestinationFolder.Location = '23,184'
 	$textBoxDestinationFolder.Size = '300,23'
+	$textBoxDestinationFolder.Text = 'Select destination folder'
 	
 	
 	# Defining destination folder select button 
 	$buttonSelectDestinationFolder = New-Object System.Windows.Forms.Button
-	$buttonSelectDestinationFolder.Text = 'Select'
-	$buttonSelectDestinationFolder.Location = '350,115'
+	$buttonSelectDestinationFolder.Location = '350,184'
+	$buttonSelectDestinationFolder.Text = 'Destination'
 	
 	# Assigning destination folder select button
 	$browseDestinationFolder = New-Object System.Windows.Forms.FolderBrowserDialog
@@ -71,7 +90,7 @@ function GenerateForm {
 	
 	# Defining copy button
 	$copyButton = New-Object System.Windows.Forms.Button
-	$copyButton.Location = '23,161'
+	$copyButton.Location = '23,230'
 	$copyButton.text = 'Copy'
 	
 	# Assigning an action to copy button
@@ -85,11 +104,14 @@ function GenerateForm {
 
 	
 	# Adding items to the form
-	$basicForm.Controls.Add($textBoxsourceFolder)
-	$basicForm.Controls.Add($buttonSelectsourceFolder)
+	$basicForm.Controls.Add($labelSourceFolder)
+	$basicForm.Controls.Add($textBoxSourceFolder)
+	$basicForm.Controls.Add($buttonSelectSourceFolder)
+	$basicForm.Controls.Add($labelDropDown)
+	$basicForm.Controls.Add($boxDropDown)
+	$basicForm.Controls.Add($labelDestinationFolder)
 	$basicForm.Controls.Add($textBoxDestinationFolder)
 	$basicForm.Controls.Add($buttonSelectDestinationFolder)
-	$basicForm.Controls.Add($boxDropDown)
 	$basicForm.Controls.Add($copyButton)
 	
 	
